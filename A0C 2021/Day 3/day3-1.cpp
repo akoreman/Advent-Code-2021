a620094474be7427199fee969804d577dbea4fad
Alice <a.koreman@outlook.com>
2022-01-23 20:43:50 +0100

    Added 3-1.

--- A0C 2021/Day 3/day3-1.cpp
+++ A0C 2021/Day 3/day3-1.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <math.h>
 
 using namespace std;
 
@@ -10,22 +11,50 @@ int main()
 {
     string line;
     ifstream myfile;
-
-    myfile.open("input.txt");
-
-
-    while ( getline (myfile,line) )
-    {
-        stringstream ss;  
-        ss << line;  
     
-
-    }
-
+    myfile.open("input.txt");
+    getline (myfile,line);
     myfile.close();
 
-    std::cout << "Answer day 3 part 1: ";
-    std::cout << " " << "\n";
+    int inputLength = line.length();
+    double inputDepth = 0;
 
+    double* Array = new double[inputLength];
+
+    double gamma = 0;
+    double epsilon = 0;
+
+    int i = 0;
+    while (i < inputLength)
+    {
+        myfile.open("input.txt");
+        Array[i] = 0;
+
+        while ( getline (myfile,line) )
+        {
+            if (i == 0)
+                inputDepth++;
+
+            stringstream ss;  
+            ss << line;  
+
+            Array[i] += (double) line[i] - 48;
+        }
+
+        if (Array[i]/inputDepth > 0.5)
+            gamma += pow(2, inputLength - 1 - i);
+        else
+            epsilon += pow(2,  inputLength - 1 - i);
+
+        i++;
+        myfile.close();
+    }
+    
+    std::cout << gamma << "\n";
+    std::cout << epsilon << "\n";
+
+    std::cout << "Answer day 3 part 1: ";
+    std::cout << gamma * epsilon << "\n";
+    
     return 0;
 }
