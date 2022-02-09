f83befaa43672d3669cf267025d3dfc77754f828
Alice <a.koreman@outlook.com>
2022-02-09 15:23:18 +0100

    Added input reading boilerplate file.

---
+++ AOC 2021/ReadInputBoilerPlate.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    string line;
+    ifstream myFile;
+    
+    myFile.open("input.txt");
+    
+    
+
+    while ( getline (myFile,line) )
+    {
+
+    }
+
+    myFile.close();
+
+    return 0;
+}
